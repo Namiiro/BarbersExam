1997d2a1de90dd870b0be42ca4519753f8d7b20e
Jackie Rose Namiiro <35915298+Namiiro@users.noreply.github.com>
2019-09-09 12:58:33 +0200

    Ændring i 'Login side'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,9 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  }
+  },
+  { path: 'maps', loadChildren: './maps/maps.module#MapsPageModule' },
+  { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' }
 ];
 
 @NgModule({
